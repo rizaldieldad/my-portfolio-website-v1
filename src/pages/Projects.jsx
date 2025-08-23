eb42d418f92db29cc8460bb133a7f89ae0664f3f
rizaldieldad <eldad.rizaldi@gmail.com>
2025-08-23 13:45:40 +0700

    fix layout and scroll behavior

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,7 @@
 import Layout from '../components/Layout'
 
 function Projects () {
-  return (
-    <Layout>
-      <div className='max-w-7xl mx-auto p-4 sm:p-8'>All Projects</div>
-    </Layout>
-  )
+  return <Layout>All Projects</Layout>
 }
 
 export default Projects
